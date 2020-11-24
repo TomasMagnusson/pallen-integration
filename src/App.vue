c25b6983b28f4c4f903b5efe89a9bebfff788e10
Tomas Magnusson <tomas@tmelektronik.se>
2020-11-24 15:09:17 +0100

    testing logout function

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <v-app id="inspire">
     <v-navigation-drawer v-model="drawer" app>
       <!--  -->
+      <a href="/.auth/logout?post_logout_redirect_uri=www.google.com">Logga ut</a>
     </v-navigation-drawer>
 
     <v-app-bar app>
