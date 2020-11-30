e0103108d0516ee750384e5a97370fc7e081f631
Tomas Magnusson <tomas@tmelektronik.se>
2020-11-30 14:14:26 +0100

    Logout experiments

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,10 @@
   <v-app id="inspire">
     <v-navigation-drawer v-model="drawer" app>
       <div>
-      <!--  -->
-      <!-- <a href="/.auth/logout?post_logout_redirect_uri=http://www.google.com">Logga ut</a> -->
-        <md-button class="md-primary" @click="logout()">Logga ut</md-button>
+        <!--  -->
+        <a href="/.auth/logout?post_logout_redirect_uri=http://www.google.com"
+          >Logga ut</a
+        >
       </div>
     </v-navigation-drawer>
 
@@ -13,26 +14,35 @@
 
       <v-toolbar-title>Pallen-integration</v-toolbar-title>
       <v-spacer></v-spacer>
-      <span>{{getUser()}}</span>
+      <span>{{ getUser() }}</span>
       <v-tooltip bottom>
-      <template v-slot:activator="{ on, attrs }">
-      <v-icon large color="blue darken-2" v-bind="attrs"
-          v-on="on">mdi-account-circle</v-icon>
-    </template>
-      <span>Inloggad användare</span>
-    </v-tooltip>
+        <template v-slot:activator="{ on, attrs }">
+          <v-icon large color="blue darken-2" v-bind="attrs" v-on="on"
+            >mdi-account-circle</v-icon
+          >
+        </template>
+        <span>Inloggad användare</span>
+      </v-tooltip>
     </v-app-bar>
 
     <v-main>
       <!--  -->
+      <div>
+        <!--  -->
+        <!-- <a href="/.auth/logout?post_logout_redirect_uri=http://www.google.com">Logga ut</a> -->
+        <md-button class="md-primary" @click="logout()">Logga ut</md-button>
+      </div>
     </v-main>
 
     <v-footer :fixed="fixed" app>
       <span style="margin-left: 24px"
-        >&copy; {{ new Date().getFullYear() }} TM Elektronik AB, Programversion: {{getVersion()}}</span
+        >&copy; {{ new Date().getFullYear() }} TM Elektronik AB, Programversion:
+        {{ getVersion() }}</span
       >
       <v-spacer></v-spacer>
-      <span style="margin-right: 20px">Byggd med VuetifyJS och Azure Static Webapps</span>
+      <span style="margin-right: 20px"
+        >Byggd med VuetifyJS och Azure Static Webapps</span
+      >
     </v-footer>
   </v-app>
 </template>
@@ -44,7 +54,7 @@ import store from "./store/store";
 
 @Component({
   name: "App",
-  store
+  store,
 })
 export default class App extends Vue {
   drawer = false;
@@ -57,18 +67,18 @@ export default class App extends Vue {
   }
 
   mounted() {
-    this.getUserInfo().then(u => {console.log(u);});
+    this.getUserInfo().then((u) => {
+      console.log(u);
+    });
   }
 
   created() {
     store.commit("profileModule/storeClientPrincipal", {
-      "identityProvider": "aad",
-      "userId": "1eeb43bacf7b463792d72e2a4324cfb0",
-      "userDetails": "tomasmagnusson1234@gmail.com",
-      "userRoles": [
-        "anonymous",
-        "authenticated"
-      ]});
+      identityProvider: "aad",
+      userId: "1eeb43bacf7b463792d72e2a4324cfb0",
+      userDetails: "tomasmagnusson1234@gmail.com",
+      userRoles: ["anonymous", "authenticated"],
+    });
   }
 
   getVersion() {
@@ -80,10 +90,7 @@ export default class App extends Vue {
   }
 
   async logout() {
-    const response = await fetch("/.auth/logout");
-    console.log(response);
     sessionStorage.clear();
   }
 }
-
 </script>
