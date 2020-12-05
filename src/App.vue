6bcecb6efb056c806cc6764f4d5dfa86e676258b
Tomas Magnusson <tomas@tmelektronik.se>
2020-12-05 18:50:27 +0100

    App.vue cleanup

--- src/App.vue
+++ src/App.vue
@@ -31,13 +31,6 @@
           </v-list-item-content>
         </v-list-item>
       </v-list>
-
-      <v-divider></v-divider>
-
-      <div>
-        <!--  -->
-        <v-btn class="md-primary" @click="logout()">Logga ut</v-btn>
-      </div>
     </v-navigation-drawer>
 
     <v-app-bar app>
@@ -57,19 +50,6 @@
     </v-app-bar>
 
     <v-main>
-      <!--  -->
-      <div>
-        <!--  -->
-        <a href="/.auth/login/aad">Logga in</a>
-      </div>
-      <div>
-        <a
-          href="/.auth/logout?post_logout_redirect_uri=https://portal.azure.com/SignOut"
-          >Logga ut</a
-        >
-        <!-- <a href="/.auth/logout?post_logout_redirect_uri=https://github.com/logout">Logga ut</a> -->
-      </div>
-
       <router-view></router-view>
     </v-main>
 
@@ -88,7 +68,6 @@
 
 <script lang="ts">
 import Vue from "vue";
-// import VueRouter from 'vue-router';
 import { Component } from "vue-property-decorator";
 import store from "./store/store";
 
@@ -102,39 +81,9 @@ export default class App extends Vue {
     { title: "Hem", icon: "mdi-home", link: "/" },
     { title: "Dashboard", icon: "mdi-view-dashboard", link: "" },
     { title: "Photos", icon: "mdi-image", link: "" },
-    { title: "About", icon: "mdi-help-box", link: "/about" },
+    { title: "Om", icon: "mdi-help-box", link: "/about" },
     { title: "Logga ut", icon: "mdi-logout", link: "/logout" },
   ];
-
-  public async getUserInfo(): Promise<any> {
-    const response = await fetch("/.auth/me");
-    const payload = await response.json();
-    const { clientPrincipal } = payload;
-    return clientPrincipal;
-  }
-
-  mounted() {
-    this.getUserInfo().then((u) => {
-      console.log(u);
-    });
-  }
-
-  created() {
-    // store.commit("profileModule/storeClientPrincipal", {
-    //   identityProvider: "aad",
-    //   userId: "1eeb43bacf7b463792d72e2a4324cfb0",
-    //   userDetails: "tomasmagnusson1234@gmail.com",
-    //   userRoles: ["anonymous", "authenticated"],
-    // });
-    // setTimeout(() =>{
-    //   store.commit("profileModule/storeClientPrincipal", {
-    //     identityProvider: "aad",
-    //     userId: "1eeb43bacf7b463792d72e2a4324cfb0",
-    //     userDetails: "windowslive@tmelektronik.se",
-    //     userRoles: ["anonymous", "authenticated"],
-    //   });
-    // }, 10000);
-  }
 
   getVersion() {
     return this.$store.state.version;
@@ -146,23 +95,6 @@ export default class App extends Vue {
 
   get roles() {
     return this.$store.getters["profileModule/ClientPrincipal"]?.userRoles;
-  }
-
-  async logout() {
-    sessionStorage.setItem("StaticWebAppsAuthCookie", "Smith");
-
-    document.cookie =
-      "StaticWebAppsAuthCookie=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-    document.cookie =
-      "has_recent_activity=1; path=/; expires=Tue, 01 Dec 2020 20:28:27 GMT; secure; HttpOnly; SameSite=Lax";
-    document.cookie =
-      "logged_in=no; domain=.github.com; path=/; expires=Wed, 01 Dec 2021 19:28:27 GMT; secure; HttpOnly; SameSite=Lax";
-    document.cookie =
-      "user_session=; path=/; max-age=0; expires=Thu, 01 Jan 1970 00:00:00 GMT; secure; HttpOnly; SameSite=Lax";
-    document.cookie =
-      "__Host-user_session_same_site=; path=/; max-age=0; expires=Thu, 01 Jan 1970 00:00:00 GMT; secure; HttpOnly; SameSite=Lax";
-    document.cookie =
-      "dotcom_user=; domain=.github.com; path=/; max-age=0; expires=Thu, 01 Jan 1970 00:00:00 GMT; secure; HttpOnly; SameSite=Lax";
   }
 }
 </script>
