d83a4fafd4dd5b9bdfe99f84e7ea887d46f28fcb
Tomas Magnusson <tomas@tmelektronik.se>
2020-12-05 20:27:00 +0100

    404 page as component

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter, { RouteConfig } from 'vue-router'
 import Home from '../views/Home.vue'
+import NotFound404Page from '../components/NotFound404Page.vue'
 
 Vue.use(VueRouter)
 
@@ -24,7 +25,8 @@ const routes: Array<RouteConfig> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  },
+  { path: '*', component: NotFound404Page }
 ]
 
 const router = new VueRouter({
