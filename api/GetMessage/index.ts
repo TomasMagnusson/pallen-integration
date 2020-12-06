2d566a9ebf8a8b980de120500ddee8b89190099f
Tomas Magnusson <tomas@tmelektronik.se>
2020-12-06 20:56:10 +0100

    azure function experiment

--- api/GetMessage/index.ts
+++ api/GetMessage/index.ts
@@ -1,7 +1,13 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
+import df = require("durable-functions");
+
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
+
+    // const client = df.getClient(context);
+    // const instanceId = await client.startNew(req.params.functionName, undefined, req.body);
+
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,28 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div>{{ message }}</div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
+    HelloWorld,
+  },
+  data: () => ({
+    message: 'Hello Vue.js!'
+  }),
+  mounted: async function() {
+      let response = await fetch(`api/message`);
+      let msg = await response.json();
+
+      this.message = msg.text;
   }
-}
+};
 </script>
